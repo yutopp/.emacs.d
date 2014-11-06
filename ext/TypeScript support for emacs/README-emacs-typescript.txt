8f2f41294cd1fa20f3cd70cef87f28637e835043
yutopp <yutopp@gmail.com>
2014-11-07 02:05:35 +0900

    update

---
+++ ext/TypeScript support for emacs/README-emacs-typescript.txt
@@ -0,0 +1,14 @@
+emacs TypeScript Readme
+----------------------------------------------------------------------------
+Author: Microsoft Open Technologies, Inc.
+
+TypeScript bundle for emacs, this bundle provides syntax highlighting and indenting.
+
+This is provided as a sample to help you get started coding with TypeScript in emacs
+
+## Feature
+1. TypeScript language syntax highlighting
+2. TypeScript indenting 
+
+## Installation
+Installation instructions can be found here: http://ergoemacs.org/emacs/emacs_installing_packages.html
--- init.el
+++ init.el
@@ -7,7 +7,6 @@
 ;;;
 (add-to-list 'load-path "~/.emacs.d/lisp")
 
-
 ;;;
 ;;; initialize packages
 (require 'package)
@@ -22,7 +21,6 @@
 ;; yfcf = yutopp config
 (defconst ytcf/required_packages
   '(
-    nzenburn-theme
     elscreen
     powerline
     highlight-parentheses
@@ -30,14 +28,28 @@
     js3-mode
     web-mode
     coffee-mode
+    zenburn-theme
     tuareg
+    go-mode
+    deferred
+    icicles
     ))
 
-(dolist (target_package ytcf/required_packages)
-  (unless (package-installed-p target_package)
-    (package-install target_package)
-    ))
+(defun ytcf/install_packages ()
+  (dolist (target_package ytcf/required_packages)
+    (unless (package-installed-p target_package)
+      (package-install target_package)
+      )))
 
+(ytcf/install_packages)
+
+;; too slow
+;(require 'deferred)
+;(deferred:$
+;  (deferred:next
+;    (lambda () (package-refresh-contents)))
+;  (deferred:nextc it
+;    (lambda () (ytcf/install_packages))))
 
 ;;;
 ;; do not create temporary files
@@ -61,6 +73,10 @@
 (setq-default visible-bell t)
 ;; dissable toolbar
 (tool-bar-mode -1)
+;;
+(menu-bar-mode 0)
+;; auto reload
+(global-auto-revert-mode 1)
 ;; set scroll bar to right
 (set-scroll-bar-mode 'right)
 ;; change cursor
@@ -77,16 +93,31 @@
  ((find-font (font-spec :name "Inconsolata-11"))
   (set-default-font "Inconsolata-11")
   (set-face-font 'variable-pitch "Inconsolata-11")
-					;(Set-Face-attribute 'default nil :height 100)
+  ;(Set-Face-attribute 'default nil :height 100)
   ))
 
 
 ;;
 (cua-mode t)
-(cond (window-system
-       (setq-default x-select-enable-clipboard t)
-       ))
+;; reference...: http://d.hatena.ne.jp/l1o0/20100429/1272557315
+;; pacman -S xclip
+;(defun my-cut-function (text &optional rest)
+;  (interactive)
+;  (let ((process-connection-type nil))
+;    (let ((proc (start-process "xclip" "*Messages*" "xclip" "-selection" "clipboard")))
+;      (process-send-string proc text)
+;      (process-send-eof proc))))
+;
+;(defun my-paste-function ()
+;  (interactive)
+;  (shell-command-to-string "xclip -o"))
+;
+;(when (executable-find "xclip")
+;  (setq interprogram-cut-function 'my-cut-function)
+;  ;(setq interprogram-paste-function 'my-paste-function)
+;  )
 (setq-default mouse-drag-copy-region nil)
+(setq x-select-enable-clipboard t)
 
 ;;
 (setq-default indent-tabs-mode nil)
@@ -135,6 +166,8 @@
 
 ;;; --
 ;;
+(require 'powerline)
+(powerline-reset)
 (powerline-default-theme)
 
 
@@ -148,17 +181,34 @@
 ;;; --
 ;; iswitchb
 ;; http://ubulog.blogspot.jp/2007/09/emacs_19.html
-(iswitchb-mode t)
-(setq read-buffer-function 'iswitchb-read-buffer)
-(setq iswitchb-regexp nil)
-(setq iswitchb-prompt-newbuffer nil)
-(add-hook 'iswitchb-define-mode-map-hook
-          '(lambda ()
-             (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
-             (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
-             (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
-             (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
+;; http://www.emacswiki.org/emacs/IswitchBuffers
+(icomplete-mode 99)
+;(iswitchb-mode 1)
+;(setq read-buffer-function 'iswitchb-read-buffer)
+;(setq iswitchb-regexp nil)
+;(setq iswitchb-prompt-newbuffer nil)
+;(add-hook 'iswitchb-define-mode-map-hook
+;          '(lambda ()
+;             (define-key iswitchb-mode-map "\C-n" 'iswitchb-next-match)
+;             (define-key iswitchb-mode-map "\C-p" 'iswitchb-prev-match)
+;             (define-key iswitchb-mode-map "\C-f" 'iswitchb-next-match)
+;             (define-key iswitchb-mode-map "\C-b" 'iswitchb-prev-match)))
 
+;;; --
+(require 'icicles)
+(icy-mode 1)
+
+
+;;; --
+(setq ido-enable-flex-matching t)
+(ido-mode 1)
+;; http://www.emacswiki.org/emacs/InteractivelyDoThings
+(defun ido-define-keys () ;; C-n/p is more intuitive in vertical layout
+  (define-key ido-completion-map (kbd "C-n") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-p") 'ido-prev-match)
+  (define-key ido-completion-map (kbd "C-f") 'ido-next-match)
+  (define-key ido-completion-map (kbd "C-b") 'ido-prev-match))
+(add-hook 'ido-setup-hook 'ido-define-keys)
 
 
 ;;; --
@@ -175,9 +225,17 @@
 ;; fold
 (require 'fold-dwim)
 
-(global-set-key (kbd "C-x n")     'fold-dwim-toggle)
+(global-set-key (kbd "C-x j")     'fold-dwim-toggle)
 (global-set-key (kbd "<M-f7>")    'fold-dwim-hide-all)
 (global-set-key (kbd "<S-M-f7>")  'fold-dwim-show-all)
+
+
+
+;;; -- window
+;(global-set-key (kbd "C-S-b")     'shrink-window-horizontally)
+;(global-set-key (kbd "C-S-f")     'enlarge-window-horizontally)
+;(global-set-key (kbd "C-S-n")     'enlarge-window-horizontally)
+;(global-set-key (kbd "C-S-p")     'shrink-window)
 
 
 
@@ -207,3 +265,9 @@
 (load "mode-config/ocaml.el")
 
 (load "mode-config/rill.el")
+
+
+;;; --
+(add-to-list 'load-path "~/.emacs.d/ext/TypeScript support for emacs")
+(add-to-list 'auto-mode-alist '("\\.ts\\'" . typescript-mode))
+(autoload 'typescript-mode "TypeScript" "Major mode for editing typescript." t)
