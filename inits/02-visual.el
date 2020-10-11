3eff9b563360bfa7d7a9d776fb3c98eb4efd4ab1
yutopp <yutopp@gmail.com>
2020-10-11 16:42:34 +0900

    Add hi-line and hiwin

--- inits/02-visual.el
+++ inits/02-visual.el
@@ -15,3 +15,23 @@
 (set-fontset-font nil 'japanese-jisx0208 (font-spec :family "IPAGothic"))
 ;;(setq face-font-rescale-alist '(("IPAGothic" . 1.08)))
 (set-frame-font (font-spec :size 18))
+
+;;; hiwin
+
+(use-package hiwin :ensure t
+  :defer t
+  :init
+  (hiwin-activate)
+  (set-face-background 'hiwin-face "gray30"))
+
+;;; hi-line
+
+;; http://rubikitch.com/2015/05/14/global-hl-line-mode-timer/
+(require 'hl-line)
+(defun global-hl-line-timer-function ()
+  (global-hl-line-unhighlight-all)
+  (let ((global-hl-line-mode t))
+    (global-hl-line-highlight)))
+(setq global-hl-line-timer
+      (run-with-idle-timer 0.02 t 'global-hl-line-timer-function))
+;; (cancel-timer global-hl-line-timer)
